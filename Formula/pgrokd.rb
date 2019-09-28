8d03b2e0dc880835cbf7ed5ee68557055ca9bd18
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-09-28 13:58:51 -0500

    Brew formula update for pgrok version v3.1.0

--- Formula/pgrokd.rb
+++ Formula/pgrokd.rb
@@ -2,24 +2,24 @@
 class Pgrokd < Formula
   desc "Server introspected tunnels to localhost a fork from pgrok."
   homepage "https://github.com/jerson/pgrok"
-  version "3.0.10"
+  version "3.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/jerson/pgrok/releases/download/v3.0.10/pgrokd_3.0.10_Darwin_x86_64.tar.gz"
-    sha256 "662d0babffa4bab2c72aa73c70fa1460d5397d16652abe65d4cf15958dbfffc0"
+    url "https://github.com/jerson/pgrok/releases/download/v3.1.0/pgrokd_3.1.0_Darwin_x86_64.tar.gz"
+    sha256 "659b4b9f3b0f7146f4cd9ec2f68a1256d01675085b1b3fbf36eb372c5121e786"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/jerson/pgrok/releases/download/v3.0.10/pgrokd_3.0.10_Linux_x86_64.tar.gz"
-      sha256 "2b7e991688bbe767fc0429dba6c269533e6d05b3a30d9ac90ef04cc1adbb6319"
+      url "https://github.com/jerson/pgrok/releases/download/v3.1.0/pgrokd_3.1.0_Linux_x86_64.tar.gz"
+      sha256 "fc13232bac9d6f7c5ccd7bf0f9a4e9293f72cb6786fbb1b37f9d7834683fb81c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jerson/pgrok/releases/download/v3.0.10/pgrokd_3.0.10_Linux_arm64.tar.gz"
-        sha256 "72d66dad711935425d9b58d934d01b28469e3ea6f0713f4517eb70f6ec71aafb"
+        url "https://github.com/jerson/pgrok/releases/download/v3.1.0/pgrokd_3.1.0_Linux_arm64.tar.gz"
+        sha256 "e5c3612a3b71b03e40b6fcacb6abe528ea552b0e58f23e17f6ef9892b877653a"
       else
-        url "https://github.com/jerson/pgrok/releases/download/v3.0.10/pgrokd_3.0.10_Linux_armv6.tar.gz"
-        sha256 "b06f8877677f04a6a2b96681b8a89163012f4e6625b1d7780e29fc4a48b9fe6c"
+        url "https://github.com/jerson/pgrok/releases/download/v3.1.0/pgrokd_3.1.0_Linux_armv6.tar.gz"
+        sha256 "9ab736a199af56c467340c07941c3b6349ab8aac0849105dcf655effe18f0f37"
       end
     end
   end
